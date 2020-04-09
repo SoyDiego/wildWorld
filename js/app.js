ec99d65b8585404c5b2e60eb93bc7e97dfd570f4
SoyDiego <diegofranchina@gmail.com>
2020-04-09 18:48:54 +0200

    Finished navbar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 const iconMenu = document.querySelector(".showIcon");
-iconMenu.addEventListener("click", () => {
-   document.querySelector(".container-menu").style.left = "0";
-} )
+const containerMenu = document.querySelector(".container-menu");
+
+iconMenu.addEventListener("click", () => openCloseMenu());
+containerMenu.addEventListener("click", () => openCloseMenu())
+
+function openCloseMenu(){
+    	if (document.querySelector(".iconMobile").classList.contains("fa-bars")) {
+		document
+			.querySelector(".iconMobile")
+			.classList.replace("fa-bars", "fa-times-circle");
+		containerMenu.style.left = "0";
+	} else {
+		document
+			.querySelector(".iconMobile")
+            .classList.replace("fa-times-circle", "fa-bars");
+            containerMenu.style.left = "-480px";
+	}
+}
